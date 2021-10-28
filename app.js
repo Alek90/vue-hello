24cda08562f7d9cd3fe854fc59a701dfe3d9f36d
Alek90 <4lek.90@gmail.com>
2021-10-28 16:26:07 +0200

    Consegna esercizio.
    Corretta tonalità dell'arancione. Aggiunti commenti concettuali e descrittivi.

--- app.js
+++ app.js
@@ -8,8 +8,13 @@
 let root = new Vue({
     el: "#root",
     data: {
+        // message = testo da inserire nel titolo: {{}}.
         message: "LOOK OUT ON THE ADVENTURE",
+
+        // image = riferimento da inserire in attributo src -> v-blind:src="".
         image: "https://motorrevu.hu/wp-content/uploads/2020/08/KTM_390adventura_motorrevu-1.jpg",
+        
+        // classi = classi css, da associare agli elementi html in attributo class -> v-blind:class="".
         classText: "header_property",
         classImg: "img_adattament"
     }
