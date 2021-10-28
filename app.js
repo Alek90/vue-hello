8971e8a36f305c80481b12c182c12bb1538dc254
Alek90 <4lek.90@gmail.com>
2021-10-28 14:56:51 +0200

    Compilazione js. Problema.
    Costruita struttura html, creata variabile con Vue, creata data, riscontrato problema a schermo.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /*  OBBIETTIVI:
     Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 */
+
+
+// Utilizziamo Vue.js:
+
+let root = new Vue({
+    el: "#root",
+    data: {
+        message: "LOOK OUT ON THE ADVENTURE"
+    }
+})
